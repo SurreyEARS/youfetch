7c302f59f96306c598618948890865fdeee8cd41
Suraj Shenoy <suraj.shenoy@live.com>
2024-07-12 14:19:05 +0100

    added cpu info

--- youfetch/youfetch.py
+++ youfetch/youfetch.py
@@ -1,7 +1,29 @@
 from requests import get
+import psutil
 
 def getpublicip():
     # Use the ifconfig.me service to get the public ip address.
     result = get("https://ifconfig.me").text
+
+    return result
+
+def getcpuinfo():
+    physcoretotal = psutil.cpu_count(logical=False)
+    coretotal = psutil.cpu_count(logical=True)
+    cpufreq = psutil.cpu_freq()
+    minfreq = cpufreq.min
+    maxfreq = cpufreq.max
+    currfreq = cpufreq.current
+    usage = psutil.cpu_percent()
+
+    result = {
+        "phystotal":physcoretotal,
+        "coretotal":coretotal,
+        "cpufreq":cpufreq,
+        "minfreq":minfreq,
+        "maxfreq":maxfreq,
+        "currfreq":currfreq,
+        "usage":usage
+    }
 
     return result
